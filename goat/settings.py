cbf606a547bb46c9282ef4d6ec71cdc64899a21f
David Lauzon <davidonlaptop@gmail.com>
2015-05-19 17:01:02 -0400

    Adding mysql database

--- goat/settings.py
+++ goat/settings.py
@@ -75,9 +75,23 @@ WSGI_APPLICATION = 'goat.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME':   os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':   'django.db.backends.mysql',
+        'HOST':     'localhost',
+        'NAME':     'biomarqueurs',
+        'USER':     'root',
+        'PASSWORD': ''
+    },
+    'optithera-hits': {
+        'ENGINE':   'django.db.backends.mysql',
+        'HOST':     'localhost',
+        'NAME':     'optiterahits',
+        'USER':     'root',
+        'PASSWORD': ''
     }
 }
 
